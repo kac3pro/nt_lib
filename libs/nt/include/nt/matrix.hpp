f35ccc224d39e79fd00b18f6e6fc46453628031a
Debian <debian@vps-96e881a4.vps.ovh.net>
2025-09-01 08:48:55 +0000

    rowReduce working (I think)

--- libs/nt/include/nt/matrix.hpp
+++ libs/nt/include/nt/matrix.hpp
@@ -1,83 +1,163 @@
 #include <vector>
 #include "nt.hpp"
-namespace Matrix {
-    
-template <typename T>
-inline T addMod(T a, T b, T m) { return (a + b) % m;}
 
 template <typename T>
-inline T mulMod(T a, T b, T m) { return (static_cast<__int128_t>(a) * b) % m;}
+inline T addMod(T a, T b, T m) { return posMod(a + b, m); }
+
 template <typename T>
-struct ModMatrix {
-    T m;
-    std::size_t width;
-    std::size_t height;
+inline T mulMod(T a, T b, T m) { return posMod(static_cast<__int128_t>(a) * b, static_cast<__int128_t>(m)); }
+template <typename T>
+inline T divMod(T a, T b, T m) { return posMod(static_cast<__int128_t>(a) * invMod(b, m), static_cast<__int128_t>(m)); }
+template <typename T>
+class ModMatrix
+{
+private:
     std::vector<T> data;
 
-    ModMatrix(T m, std::size_t width, std::size_t height, std::vector<T> data) : 
-                    m(m), width(width), height(height), data(std::move(data)) {
-                        if (width*height != data.size())
-                            throw std::invalid_argument("data size != width*height");
+public:
+    T m;
+    std::size_t width, height;
+
+    ModMatrix(T m, std::size_t width, std::size_t height, std::vector<T> data) : m(m), width(width), height(height), data(data)
+    {
+        if (width * height != data.size())
+            throw std::invalid_argument("data size != width*height");
+        for (auto &x : this->data)
+            x = posMod(x, m);
     }
-    std::span<T> operator[] (std::size_t index) {
-        if (index>=height) throw std::out_of_range("row");
-        return {data.data()+index*width, width};
+    std::span<T> operator[](std::size_t index)
+    {
+        if (index >= height)
+            throw std::out_of_range("row");
+        return {data.data() + index * width, width};
     }
-    ModMatrix<T> &switchRows(size_t i, size_t j) {
+    std::span<const T> operator[](std::size_t index) const
+    {
+        if (index >= height)
+            throw std::out_of_range("row");
+        return {data.data() + index * width, width};
+    }
+    ModMatrix<T> &swapRows(size_t i, size_t j)
+    {
         if (i >= height || j >= height)
             throw std::out_of_range("row index out of range");
-        if (i == j) return *this;                 // nothing to do
+        if (i == j)
+            return *this; // nothing to do
         auto it1 = data.begin() + i * width;
         auto it2 = data.begin() + j * width;
 
-        std::swap_ranges(it1, it1 + width, it2);   // swap the entire rows in O(cols)
+        std::swap_ranges(it1, it1 + width, it2);
         return *this;
     }
-    ModMatrix<T> &addNTimesIthRowtoJthRow(T n, size_t i, size_t j) {
-        auto &rowi = (*this)[i];
-        auto &rowj = (*this)[j];
-        for (size_t k = 0; k < rowi.size(); ++k) {
+    ModMatrix<T> &addNTimesIthRowtoJthRow(T n, size_t i, size_t j)
+    {
+        const auto rowi = (*this)[i];
+        auto rowj = (*this)[j];
+        for (size_t k = 0; k < rowi.size(); ++k)
+        {
             auto &ai = rowi[k];
             auto &aj = rowj[k];
-            ai = addMod(ai, mulMod(n, aj, m), m);
+            aj = addMod(aj, mulMod(n, ai, m), m);
         }
         return (*this);
     }
-    ModMatrix<T> &mulRow(T n, size_t i) {
-        auto &rowi = (*this)[i];
+    ModMatrix<T> &mulRow(T n, size_t i)
+    {
+        auto rowi = (*this)[i];
         for (auto &ai : rowi)
             ai = mulMod(ai, n, m);
         return (*this);
     }
-    
-    ModMatrix<T> &rowReduce() {
+
+    ModMatrix<T> &rowReduce()
+    {
         if (!isPrime(m))
-            throw std::runtime_error("rowReduce works only form prime moduli");
-        
-        
-        
+            throw std::runtime_error("rowReduce works only for prime moduli");
+        for (size_t j = 0; j < width; ++j)
+        { // for each column
+            size_t pivotRowIndex = -1;
+            for (size_t i = j; i < height; ++i)
+            {
+                if ((*this)[i][j] != 0)
+                    pivotRowIndex = i; // find a row with a nonzero entry in that column
+            }
+            if (pivotRowIndex == -1) continue; // if you can't, continue
+            mulRow(invMod((*this)[pivotRowIndex][j], m), pivotRowIndex);
+            for (size_t i = 0; i < height; ++i)
+            { // for every other row
+                if (i == pivotRowIndex)
+                    continue;
+                addNTimesIthRowtoJthRow(-(*this)[i][j], pivotRowIndex, i);// subtract accordingly
+            }
+            if (j < height)
+                swapRows(j, pivotRowIndex);
+        }
+        return (*this);
+    }
+    static ModMatrix<T> I(size_t d, T m)
+    {
+        ModMatrix<T> a(m, d, d, std::vector<T>(d * d, 0));
+        for (size_t i = 0; i < d; ++i)
+        {
+            a[i][i] = 1;
+        }
+        return a;
     }
 };
 template <typename T>
-inline bool isSameSize(const ModMatrix<T> &left, const ModMatrix<T> &right) {
+std::ostream &operator<<(std::ostream &os, ModMatrix<T> const &m)
+{
+    const std::size_t maxShow = 4;
+    os << "ModMatrix(" << m.width << "x" << m.height << ") " << std::endl;
+    for (size_t i = 0; i < m.height; ++i)
+    {
+        for (size_t j = 0; j < m.width; ++j)
+        {
+            os << m[i][j] << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+template <typename T>
+inline bool isSameSize(const ModMatrix<T> &left, const ModMatrix<T> &right)
+{
     return left.height == right.height && left.width == right.width;
 }
 template <typename T>
-inline bool canAdd(const ModMatrix<T> &left, const ModMatrix<T> &right) {
+inline bool canAdd(const ModMatrix<T> &left, const ModMatrix<T> &right)
+{
     return left.m == right.m && isSameSize(left, right);
 }
 template <typename T>
-inline bool canMultiply(const ModMatrix<T> &left, const ModMatrix<T> &right) {
+inline bool canMultiply(const ModMatrix<T> &left, const ModMatrix<T> &right)
+{
     return left.m == right.m && left.width == right.height;
 }
 template <typename T>
-ModMatrix<T> operator+(const ModMatrix<T> &left, const ModMatrix<T> &right) {
-        if(!isSameSize(left,right))
-            throw std::invalid_argument("sizes don't match");
-        std::vector<T> arr(left.data.size());
-        for (size_t i = 0; i < left.data.size(); ++i) {
-            arr[i] = addMod(left.data[i], right.data[i], left.m);
+ModMatrix<T> operator+(const ModMatrix<T> &left, const ModMatrix<T> &right)
+{
+    if (!isSameSize(left, right))
+        throw std::invalid_argument("sizes don't match");
+    size_t size = left.width * left.height;
+    ModMatrix<T> a(left.m, left.width, left.height, std::vector<T>(size, 0));
+    for (int i = 0; i < left.height; ++i)
+        for (size_t j = 0; j < left.width; ++j)
+            a[i][j] = left[i][j] + right[i][j];
+    return a;
+}
+template <typename T>
+bool operator==(const ModMatrix<T> &left, const ModMatrix<T> &right)
+{
+    if (!isSameSize(left, right) || left.m != right.m)
+        return false;
+    for (size_t i = 0; i < left.height; ++i)
+    {
+        for (size_t j = 0; j < left.width; ++j)
+        {
+            if ((left[i][j] - right[i][j]) % left.m != 0)
+                return false;
         }
-        return ModMatrix<T>(left.m, left.width, left.height, std::move(arr));
     }
+    return true;
 }
--- tests/nt_test/matrix_test.cpp
+++ tests/nt_test/matrix_test.cpp
@@ -1,5 +1,66 @@
 #include "gtest/gtest.h"
 #include "nt/matrix.hpp"
-TEST(Test1, test1) {
-    ASSERT_TRUE(true);
+TEST(TestAdd, test) {
+    ASSERT_EQ(ModMatrix(2, 2, 2, {1,0,0,1}) + ModMatrix(2,2,2,{1,1,1,1}), ModMatrix(2, 2, 2, {0,1,1,0}));
+}
+
+ModMatrix a(13,3,3, {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9 
+});
+ModMatrix b(1'000'000'007LL,3,2, {
+    2, 4, 1,
+    4, 0, 1,
+});
+ModMatrix c(13,2,3, {
+    1, 2, 
+    4, 5,
+    7, 8,
+});
+ModMatrix d(2137,3,3, {
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 11 
+});
+
+template <typename T>
+bool verifyRowReduced(const ModMatrix<T> &a) {
+    int previousPivot = -1;
+    for (size_t i = 0; i < a.height; ++i) {
+        for (int j = 0; j < a.width; ++j) {
+            if (a[i][j] != 0) {
+                if(a[i][j] != 1  || j <= previousPivot )
+                    return false;
+                else {
+                    previousPivot = j;
+                    break;
+                }
+            }
+            // if (j == a.width - 1)
+                // previousPivot  = a.width;
+        }
+    }
+    return true;
+
+}
+TEST(Identity, test) {
+    ASSERT_EQ(b, ModMatrix<long long>::I(3,5));
+}
+TEST(ElementaryOps, squareBracket) {
+    ASSERT_EQ(a[0][0], 1);
+    ASSERT_EQ(a[0][1], 2);
+    ASSERT_EQ(a[0][2], 3);
+    ASSERT_EQ(a[1][0], 4);
+}
+TEST(ElementaryOps, mulRow) {
+    ASSERT_EQ(a.mulRow(2, 0)[0][0], 2);
+    ASSERT_EQ(a[0][1], 4);
+    ASSERT_EQ(a[0][2], 1);
+}
+TEST(RowReduce, small) {
+    ASSERT_TRUE(verifyRowReduced(a.rowReduce()));
+    ASSERT_TRUE(verifyRowReduced(b.rowReduce()));
+    ASSERT_TRUE(verifyRowReduced(c.rowReduce()));
+    ASSERT_EQ(d.rowReduce(), ModMatrix<int>::I(3, 2137));
 }
